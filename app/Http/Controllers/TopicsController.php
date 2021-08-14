e95dba9b58c97a8eab0a6532838d98771bed5200
vagrant <vagrant@homestead>
2021-08-14 03:33:48 +0000

    用户可以创建话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,22 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TopicRequest;
+use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
-
+use Auth;
 class TopicsController extends Controller
 {
+    public function __construct()
+    {
+
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     public function index(Request $request)
     {
         $topics=Topic::withOrder($request->order)->with('user','category')->paginate(10);
         return view('topics.index',compact('topics'));
     }
-    public function create()
+    public function create(Topic $topic)
     {
-        return 'create';
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
     }
-    public function store()
+    public function store(TopicRequest $request,Topic $topic)
     {
-
+        $topic->fill($request->all());
+        $topic->user_id=Auth::id();
+        $topic->save();
+        return redirect()->route('topics.show',$topic->id)->with('success','帖子创建成功');
+    }
+    public function show()
+    {
+        return 1;
     }
 }
