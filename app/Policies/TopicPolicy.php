8c669c76c942a4ec80d6fddb5c664c36ef58898a
vagrant <vagrant@homestead>
2021-08-17 03:51:49 +0000

    用户可以编辑话题

---
+++ app/Policies/TopicPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TopicPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function update(User $currentuser,Topic $topic)
+    {
+        return $currentuser->id===$topic->user_id;
+    }
+}
