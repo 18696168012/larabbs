6d7d7411c7994dbc7704d218f015f4dd779bc796
vagrant <vagrant@homestead>
2021-08-27 07:07:27 +0000

    Simditor 编辑器字体

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -82,6 +82,7 @@
                     leaveConfirm: '文件上传中，关闭此页面将取消上传。'
                 },
                 pasteImage: true,
+                toolbar: ['title', 'bold', 'italic', 'underline', 'strikethrough', 'fontScale', 'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|', 'link', 'image', 'hr', '|', 'indent', 'outdent', 'alignment'],
             });
         });
     </script>
